bdbb062c3ff8fbe023e60e7f53efd330aa393771
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-07 16:43:55 +0700

    Mengisi data menggunakan cin Akses nested: mhs.alamat.desa mhs.alamat.kota

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -17,4 +17,17 @@ int main()
     // Deklarasi obyek dari struct orang
     orang mhs;
 
+    // isi obyek
+    cout << "Nama Mahasiswa:";
+    cin >> mhs.nama;
 
+    cout << "Alamat Desa:";
+    cin >> mhs.alamat.desa;
+
+    cout << "Alamat Kota:";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur:";
+    cin >> mhs.umur;
+
+    cout << endl;
