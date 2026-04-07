23f772ce9d2fead6d7cf922f8b1e22be59cfc0cc
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-07 16:41:08 +0700

    DetailAlamat → menyimpan desa & kota orang → memiliki: nama alamat (struct lain) umur

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat{
+    string desa;
+    string kota;
+};
+
+struct orang{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
