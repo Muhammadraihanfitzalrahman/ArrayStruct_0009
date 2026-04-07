0a15440b30592fa8ad0a769ed77448c30037cbb5
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-07 16:33:26 +0700

    Menggunakan for untuk input 3 mahasiswa getline → supaya bisa input dengan spasi cin.ignore() → penting agar input tidak loncat

--- StructArray.cpp
+++ StructArray.cpp
@@ -19,3 +19,25 @@ int main()
 {
     // Deklarasi array dari struct orang
     orang mhs[3];
+
+    cin.ignore(); // penting sebelum getline pertama
+
+    // Mengisi Objek
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "Data ke-" << i + 1 << endl;
+
+        cout << "Nama Mahasiswa: ";
+        getline(cin, mhs[i].nama);
+
+        cout << "Alamat Desa: ";
+        getline(cin, mhs[i].alamat.desa);
+
+        cout << "Alamat Kota: ";
+        getline(cin, mhs[i].alamat.kota);
+
+        cout << "Umur: ";
+        cin >> mhs[i].umur;
+        cin.ignore(); // supaya getline berikutnya tidak ke-skip
+
+    }
